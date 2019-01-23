090532a13479c52c46f1856c957ca1e0c0119c91
Pedro Nicolas <pedro_bnf_96@hotmail.com>
2019-01-23 16:15:46 +0100

    added loginview component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginViewComponent } from './login-view/login-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login', 
+  component: LoginViewComponent  
+},
+{
+  path: '', 
+  component: LoginViewComponent
+},
+{
+  path: '**', 
+  redirectTo:'login', 
+  pathMatch: 'full',
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
